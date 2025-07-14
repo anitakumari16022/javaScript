977c03bc84f556951fdbf16ede5f041c32fadf99
anitakumari16022 <anitakumari90972@gmail.com>
2025-07-14 11:54:34 +0000

    commit chnages

---
+++ 01_basics/first.js
@@ -0,0 +1,7 @@
+const accountId= 123446
+let accountEmail='anita@123gmail.com'
+var accountPaasword="3567"
+accountcity='Jaipuer'
+// accountId=67 //not  allowed because it is const
+console.table([accountId,accountEmail,accountPaasword,accountcity])
+// to print th data in single table//
