7afb6a3bab85e0792adb3cc9087f41b5444b461c
anitakumari16022 <anitakumari90972@gmail.com>
2025-07-19 17:58:07 +0000

    comparison of datatypes

---
+++ 01_basics/comparison.js
@@ -0,0 +1,25 @@
+// console.log(2>3)
+// console.log(3>=2)
+// console.log(3<=2)
+// console.log(3!=2)
+
+console.log(null>0)//sometime convert the null into naan or sometime into 0 for 
+console.log(null==0) // comparison ,which arises the issue
+console.log(null>=0)
+
+//there is difference between comparison and in to equal
+ // "===" strict equal checks the value as well its datatype
+console.log("2"===2) //checked its datatype one is string and other is number so operation will not performed
+
+
+
+
+
+
+
+
+
+
+
+
+
