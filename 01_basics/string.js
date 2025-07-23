a3a6f507075931563e44c81beeabe7ab19832f6c
anitakumari16022 <anitakumari90972@gmail.com>
2025-07-23 17:23:47 +0000

    string functions

---
+++ 01_basics/string.js
@@ -0,0 +1,22 @@
+const name='anita'
+const sirname='kumari'
+console.log(`  My name is ${name} ${sirname}`)
+console.log(name[0])
+console.log(name.length)
+
+const nname=name.substring(2,5)
+console.log(nname)
+//slice can reverse the string.
+const fname=name.slice(-5,3)
+console.log(fname)
+
+const newString="  Anita "
+console.log(newString)
+const rstring=newString.trim()//removes whhite spaces from front and end
+
+console.log(rstring)
+
+//replac(replace valued,new value o be replaced with)
+
+
+
