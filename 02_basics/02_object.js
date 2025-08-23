a8125533fa533e2595ed63502c793aa1c4352002
anitakumari16022 <anitakumari90972@gmail.com>
2025-08-23 17:38:39 +0000

    object

--- 02_basics/02_object.js
+++ 02_basics/02_object.js
@@ -1,10 +1,10 @@
 //singelton 
 //const tinder=new Object() // declaring object through object function
-const tinder={}//another method to declare singelton object
+//const tinder={}//another method to declare singelton object
 
 tinder.userID="1233"
 tinder.name="anoita"
-console.log(tinder);
+//console.log(tinder);
 
 const details={
    name :"anita",
@@ -16,4 +16,22 @@ const details={
 
    }
 }
-console.log(details.fullname.userfullname.firstname);
+//console.log(details.fullname.userfullname.firstname);
+const obj1={
+    1:"a",
+    2:"b"
+}
+const obj2={
+    1:"p",
+    2:"q"
+}
+//const sum=object.assign({},obj1,obj2);
+//{}=works as target in assigning wheras other object works as source which will add 
+const obj3= {...obj1,...obj2} // best method for add in single object
+//console.log(obj3);
+
+//[]array
+//{} object declaration
+
+
+
